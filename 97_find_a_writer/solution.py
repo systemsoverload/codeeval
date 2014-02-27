f7b95928c8bb7b6474059e2a75bb6d9538a33a05
TJ Kells <tjkells@gmail.com>
2014-02-27 12:46:27 -0600

    Add solution to #97

---
+++ 97_find_a_writer/solution.py
@@ -0,0 +1,15 @@
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    lines = [line.strip() for line in f.readlines() if line.strip()]
+
+for l in lines:
+    encoded_str, c = l.split('|')
+    code = c.split(' ')
+    author = []
+    for key in code:
+    	if key:
+    		author.append(encoded_str[int(key) - 1])
+
+    print ''.join(author)
+
